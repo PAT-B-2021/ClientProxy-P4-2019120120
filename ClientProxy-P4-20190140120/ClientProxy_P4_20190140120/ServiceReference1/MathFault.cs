ea54ad9c98fe35f2bef64dc6d05c69c74a2350b8
AlvitoAuliansyah <alauliansyah01@gmail.com>
2021-11-22 23:55:19 +0700

    Add project files.

---
+++ ClientProxy-P4-20190140120/ClientProxy_P4_20190140120/ServiceReference1/MathFault.cs
@@ -0,0 +1,8 @@
+﻿namespace ClientProxy_P4_20190140126.ServiceReference1
+{
+    internal class MathFault
+    {
+        public bool Pesan { get; internal set; }
+        public bool Kode { get; internal set; }
+    }
+}
